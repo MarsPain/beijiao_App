6aebc10a86c205067fd311748f962be98deb988d
H <1434579002@qq.com>
2017-11-21 15:28:38 +0800

    第三页选项卡以及点击回复按钮的上卷下拉效果。

---
+++ js/tab-classification.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+//	页面滚动效果
+	mui(".mui-scroll-wrapper").scroll({
+		deceleration : 0.0005
+	});
+	
+//	选项卡切换效果
+	$("#first_tab_option").click(function() {
+	  $('#second_tab').hide();
+	  $('#third_tab').hide();
+	  $('#first_tab').show();
+	});
+	
+	$("#second_tab_option").click(function() {
+	  $('#first_tab').hide();
+	  $('#third_tab').hide();
+	  $('#second_tab').show();
+	});
+	
+	$("#third_tab_option").click(function() {
+	  $('#first_tab').hide();
+	  $('#second_tab').hide();
+	  $('#third_tab').show();
+	});
+	
+//	第三页选项卡中回复的上卷下拉效果
+	$(".answer").click(function(){
+		if($(this).text() == "查看回复"){
+			$(this).text("关闭回复");
+			$(this).nextAll().slideDown("fast", function(){
+	
+			});
+		}else{
+			$(this).text("查看回复");
+			$(this).nextAll().slideUp("fast", function(){
+	
+			});
+		}
+	})
+}
