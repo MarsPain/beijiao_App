7cecb032a1fad2ec2e387f19d177228c21018b1a
H <1434579002@qq.com>
2017-11-21 10:42:55 +0800

    分类浏览页面选项卡框架和按钮样式

--- js/tab-main.js
+++ js/tab-main.js
@@ -5,4 +5,10 @@ window.onload = function(){
 		deceleration : 0.0005
 	});
 	
+	//首页图片轮播效果
+	var img_policy = mui('#img_policy');
+		img_policy.slider({
+			interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+	});
+	
 }
