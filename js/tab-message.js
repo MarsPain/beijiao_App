28e819a32b69bb3a9c9681a0084926048447fed6
H <1434579002@qq.com>
2017-11-22 11:32:33 +0800

    消息页面的样式完成

--- js/tab-message.js
+++ js/tab-message.js
@@ -7,21 +7,26 @@ window.onload = function(){
 //	选项卡切换效果
 	$("#first_tab_option").click(function() {
 	  $('#second_tab').hide();
-	  $('#third_tab').hide();
 	  $('#first_tab').show();
 	});
 	
 	$("#second_tab_option").click(function() {
 	  $('#first_tab').hide();
-	  $('#third_tab').hide();
 	  $('#second_tab').show();
 	});
 	
-	$("#third_tab_option").click(function() {
-	  $('#first_tab').hide();
-	  $('#second_tab').hide();
-	  $('#third_tab').show();
-	});
+//	第二页选项卡中点击查看回复的上卷下拉效果
+	$(".answer").click(function(){
+		if($(this).text() == "查看回复"){
+			$(this).text("关闭回复");
+			$(this).nextAll().slideDown("fast", function(){
 	
-
+			});
+		}else{
+			$(this).text("查看回复");
+			$(this).nextAll().slideUp("fast", function(){
+	
+			});
+		}
+	});
 }
