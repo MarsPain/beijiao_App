dc3470174fc5314261b74f635ff3fb00f3b2c165
H <1434579002@qq.com>
2017-11-28 15:52:39 +0800

    优化我的消息页面的选项卡切换以及点击上卷下拉的响应速度

--- js/tab-message.js
+++ js/tab-message.js
@@ -5,18 +5,18 @@ window.onload = function(){
 	});
 	
 //	选项卡切换效果
-	$("#first_tab_option").click(function() {
+	mui('body').on('tap','#first_tab_option',function() {
 	  $('#second_tab').hide();
 	  $('#first_tab').show();
 	});
 	
-	$("#second_tab_option").click(function() {
+	mui('body').on('tap','#second_tab_option',function() {
 	  $('#first_tab').hide();
 	  $('#second_tab').show();
 	});
 	
 //	第二页选项卡中点击查看回复的上卷下拉效果
-	$(".answer").click(function(){
+	mui('body').on('tap','.answer',function(){
 		if($(this).text() == "查看回复"){
 			$(this).text("关闭回复");
 			$(this).nextAll().slideDown("fast", function(){
